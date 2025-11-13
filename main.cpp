4e55599fcbecd303d297aee7df709e1ee05570e1
Alexey Yaroshenko <alexy.yaroshenko05@icloud.com>
2025-11-13 23:24:50 +0300

    excelent rationals

--- main.cpp
+++ main.cpp
@@ -8,57 +8,67 @@
 class Rational{
 protected:
     void eq_denom(Rational& fr, Rational& sc){
-        if(fr.denom > 0 && sc.denom < 0 || fr.denom < 0 && sc.denom > 0){
-            sc.denom *= -1;
-            sc.num *= -1;
-        }
-
-        if(fr.denom != sc.denom){
-            int temp_d = fr.denom;
-            fr.num *= sc.denom;
-            fr.denom *= sc.denom;
-            sc.num *= temp_d;
-            sc.denom *= temp_d;
-        }
-    }
-
-    void normalize(Rational& fr){
-        if (fr.denom == 0)
-            throw "Denominator can't be zero";
-        if (fr.num == 0) {
-            fr.denom = 1;
-            return;
-        }
-        int gcd_num = gcd(fr.num, fr.denom);
-        fr.num /= gcd_num;
-        fr.denom /= gcd_num;
-
         if (fr.denom < 0) {
             fr.num = -fr.num;
             fr.denom = -fr.denom;
         }
+        if (sc.denom < 0) {
+            sc.num = -sc.num;
+            sc.denom = -sc.denom;
+        }
+
+        if(fr.denom != sc.denom){
+            auto lcm_num = fr.denom * sc.denom / gcd(fr.denom, sc.denom);
+
+            fr.num *= lcm_num / fr.denom;
+            fr.denom *= lcm_num / fr.denom;
+            sc.num *= lcm_num / sc.denom;
+            sc.denom *= lcm_num / sc.denom;
+        }
     }
 
-    int gcd(int a, int b){
-        a = std::abs(a);
-        b = std::abs(b);
+    void normalize(){
+        if (this->denom < 0) {
+            this->num = -this->num;
+            this->denom = -this->denom;
+        }
+        if (this->denom == 0)
+            throw std::invalid_argument("Denominator can't be zero");
+        if (this->num == 0) {
+            this->denom = 1;
+            return;
+        }
+        auto gcd_num = gcd(this->num, this->denom);
+        this->num /= gcd_num;
+        this->denom /= gcd_num;
+    }
+
+    long long int gcd(long long int a, long long int b){
+        a = std::abs(a); b = std::abs(b);
 
         while(b != 0){
-            int temp = b;
-            b = a % b;
-            a = temp;
+            int temp = a % b;
+            a = b;
+            b = temp;
         }
+
         return a;
     }
+
+    
 public:
-    long int num;
-    long int denom;
+    long long int num;
+    long long int denom;
 
-    Rational(long int n) : num{n}, denom{1} { };
+    Rational(long long int n) : num{n}, denom{1} { };
 
-    Rational(long int n, long int d){
+    Rational(long long int n, long long int d){
         if(d == 0)
-            throw "Denominator can't be zero\n";
+            throw std::invalid_argument("Denominator can't be zero");
+        if(d < 0){
+            d *= -1;
+            n *= -1;
+        }
         num = n;
         denom = d;
     }
@@ -71,7 +81,7 @@ public:
 
         eq_denom(temp_this, temp_other);
         Rational answer(temp_this.num + temp_other.num, temp_this.denom);
-        normalize(answer);
+        answer.normalize();
 
         return answer;
     }
@@ -82,21 +92,21 @@ public:
 
         eq_denom(temp_this, temp_other);        
         Rational answer(temp_this.num - temp_other.num, temp_this.denom);
-        normalize(answer);
+        answer.normalize();
 
         return answer;
     }
 
     Rational operator * (const Rational& other){
         Rational answer(num * other.num, denom * other.denom);
-        normalize(answer);
+        answer.normalize();
 
         return answer;
     }
 
     Rational operator / (const Rational& other){
         Rational answer(num * other.denom, denom * other.num);
-        normalize(answer);
+        answer.normalize();
 
         return answer;
     }
@@ -106,7 +116,7 @@ public:
             num = other.num;
             denom = other.denom;
         }
-        normalize(*this);
+        this->normalize();
 
         return *this;
     }
@@ -114,7 +124,7 @@ public:
     Rational& operator = (const long int& other){
         num = other;
         denom = 1;
-        normalize(*this);
+        this->normalize();
 
         return *this;
     }
@@ -122,7 +132,7 @@ public:
     Rational& operator *= (const Rational& other){
         num *= other.num;
         denom *= other.denom;
-        normalize(*this);
+        this->normalize();
 
         return *this;
     }
@@ -133,7 +143,7 @@ public:
 
         eq_denom(temp_this, temp_other);        
         temp_this.num += temp_other.num;
-        normalize(temp_this);
+        temp_this.normalize();
         *this = temp_this;
 
         return *this;
@@ -145,26 +155,28 @@ public:
 
         eq_denom(temp_this, temp_other);        
         temp_this.num -= temp_other.num;
-        normalize(temp_this);
+        temp_this.normalize();
         *this = temp_this;
         
         return *this;
     }
 
-    bool operator < (Rational& other){
-        eq_denom(*this, other);
-        bool ans = num < other.num;
-        normalize(*this);
-        normalize(other);
+    bool operator < (const Rational& other){
+        Rational temp_this = *this;
+        Rational temp_other = other;
+
+        eq_denom(temp_this, temp_other);
+        bool ans = temp_this.num < temp_other.num;
     
         return ans;
     }
 
-    bool operator == (Rational other){
-        eq_denom(*this, other);
-        bool ans = num == other.num;
-        normalize(*this);
-        normalize(other);
+    bool operator == (const Rational& other){
+        Rational temp_this = *this;
+        Rational temp_other = other;
+
+        eq_denom(temp_this, temp_other);
+        bool ans = temp_this.num == temp_other.num;
     
         return ans;
     }
@@ -180,13 +192,7 @@ public:
     }
 };
 
-long int det(float** A, int n);
-
-long int M(float** A, int n, int J, int I = 0);
-
 Rational det_Gausses(std::vector<std::vector<Rational>> A, int n);
-
-void new_main();
 
 int main() {
     srand(time(0));
@@ -206,8 +212,6 @@ int main() {
         std::cout << '\n';
     }
 
-    std::cout << '\n' << "====TRIANG====\n";
-
     auto start_time = std::chrono::high_resolution_clock::now();
 
     Rational ans = det_Gausses(A, N);
@@ -220,64 +224,12 @@ int main() {
     return 0;
 }
 
-long int det(float** A, int n){
-
-    long int det = 0;
-
-    if(n < 1){
-        throw "Unable to find a det";
-    }
-    else if(n == 1){
-        return A[0][0];
-    }
-    else if(n == 2){
-        return A[0][0] * A[1][1] - A[0][1] * A[1][0];
-    }
-    else{
-        for(int j = 0; j < n; j++){
-            long int Min = M(A, n, j);
-            det += pow(-1, j) * A[0][j] * Min;
-        }
-    }
-
-    return det;
-}
-
-long int M(float** A, int n, int J, int I){
-
-    int ki = 0, kj = 0;
-
-    float** B = new float*[n-1]{};
-    for(size_t i = 0; i < n-1; i++)
-        B[i] = new float[n-1];
-
-    for(size_t i = 0; i < n-1; i++){
-        kj = 0;
-        if(i == I)
-            ki = 1;
-        for(size_t j = 0; j < n-1; j++){
-            if(j == J)
-                kj = 1;
-            B[i][j] = A[i+ki][j+kj];
-        }
-    }
-
-    long int M = det(B, n-1);
-
-    for(size_t i = 0; i < n-1; i++){
-        delete[] B[i];
-    }
-    delete[] B;
-
-    return M;
-}
-
 Rational det_Gausses(std::vector<std::vector<Rational>> A, int n){
     Rational det = 1;
 
     for(size_t i = 0; i < n; i++){
         Rational max_val = A[i][i];
-        int row_max_val = i;
+        auto row_max_val = i;
         for(size_t k = i; k < n; k++){
             if(max_val < A[k][i]){
                 max_val = A[k][i];
@@ -293,31 +245,25 @@ Rational det_Gausses(std::vector<std::vector<Rational>> A, int n){
         det *= A[i][i];
 
         try{
-            // std::cout << "Coef: ";
             for(size_t t = i+1; t < n; t++){
-                // if(A[t][i] == (Rational(0)))
-                //     continue;
                 Rational coef = A[t][i] / A[i][i];
-                std::cout << coef << ' ';
                 for(size_t k = i; k < n; k++){
                     auto at = coef * A[i][k];
                     A[t][k] -= at;
                 }
             }
-            // std::cout << '\n';
         }
         catch(const std::exception& e){
             std::cout << e.what() << '\n';
         }
 
-        std::cout << "Stage: " << i << '\n';
+        std::cout << "\n====STAGE: " << i << " ====\n";
         for(size_t k = 0; k < n; k++){
-            for(size_t j = 0; j < n; j++){
-                std::cout << A[k][j] << ' ';
+            for(size_t l = 0; l < n; l++){
+                std::cout << A[k][l] << ' ';
             }
             std::cout << '\n';
         }
-        std::cout << '\n';
     }
 
     std::cout << "\n\n" << "=====FINAL=====\n";
